eb813128339d4acd48eb72aa1c3696fd1ba9f217
sorenisanerd <78519591+sorenisanerd@users.noreply.github.com>
2021-02-18 16:13:35 +0100

    Play song when receiving stuff

--- main.ts
+++ main.ts
@@ -6,6 +6,17 @@ radio.onReceivedString(function (receivedString) {
     Math.map(radio.receivedPacket(RadioPacketProperty.SignalStrength), -128, -42, 0, 100),
     100
     )
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
 })
 radio.setGroup(21)
 basic.forever(function () {
